62f61a251bfcba4b3ed3256fdf4027d11150396a
Lydia Strebe <lydia.strebe@gmail.com>
2020-03-10 09:59:40 -0500

    Work done early 3/10

--- Classwork/M2/ClassRoster/src/test/java/com/lds/classroster/service/ClassRosterServiceLayerTest.java
+++ Classwork/M2/ClassRoster/src/test/java/com/lds/classroster/service/ClassRosterServiceLayerTest.java
@@ -18,6 +18,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -28,10 +30,15 @@ public class ClassRosterServiceLayerTest {
     private ClassRosterServiceLayer service;
 
     public ClassRosterServiceLayerTest() {
-        ClassRosterDao dao = new ClassRosterDaoStubImpl();
-        ClassRosterAuditDao auditDao = new ClassRosterAuditDaoStubImpl();
+//        ClassRosterDao dao = new ClassRosterDaoStubImpl();
+//        ClassRosterAuditDao auditDao = new ClassRosterAuditDaoStubImpl();
+//
+//        service = new ClassRosterServiceLayerImpl(dao, auditDao);
 
-        service = new ClassRosterServiceLayerImpl(dao, auditDao);
+        ApplicationContext ctx
+                = new ClassPathXmlApplicationContext("applicationContext.xml");
+        service
+                = ctx.getBean("serviceLayer", ClassRosterServiceLayer.class);
     }
 
     @BeforeAll
